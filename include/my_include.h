5bfab7213b3e5438a1cb72ad2391e006745388ce
abhish1ss <abhishekssiin@gmail.com>
2021-07-25 19:04:36 +0530

    changing fprint in include, output file to binary

--- include/my_include.h
+++ include/my_include.h
@@ -1,12 +1,12 @@
-#include<stdio.h>
-#include<limits.h>
-#include<stdbool.h>
-#include<stdlib.h>
-#include<string.h>
-
+#include <stdio.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 
 char argString[1024] = ""; 
 
 #define PRINT_LOG(...)           \
         sprintf(argString, __VA_ARGS__);    \
-        fprintf(stderr, "%s|%d|%s|%s\n", __FILE__, __LINE__, __func__, argString); 
+        fprintf(stdout, "%s|%d|%s|%s\n", __FILE__, __LINE__, __func__, argString); 
